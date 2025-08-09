d49c05ddbb5000edac32ae71ad85f7491ea9f6ca
damodara2006 <damodara2006@gmail.com>
2025-08-09 22:08:27 +0530

    feat: update project title to include additional context

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> KRISH AND DAMO  CI & CD PROJECT : UPDATE 1</h1>
+      <h1> KRISH AND DAMO  CI & CD PROJECT : UPDATE 1 Adiye ithu na panathu</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
